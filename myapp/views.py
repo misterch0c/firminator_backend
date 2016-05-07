f7a006cdefbdd1ec3e6936c8bf9b6fcf28f75cbd
misterch0c <mstrch0c@gmail.com>
2016-05-07 15:42:29 -0700

    extractor import

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,8 @@
+import subprocess
+
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
-
+from myapp.extractor import Extractor, ExtractionItem
 from django.http import HttpResponse
 from myapp.models import Image, Product, Brand
 #import datetime
@@ -46,6 +48,7 @@ MODEL:
 """
 def get_brand(brand):
     b = Brand.objects.filter(name__icontains=brand)
+    #Brand id 99 is for all unknown ones
     if not b:
         return 99
     else:
@@ -55,6 +58,8 @@ def get_brand(brand):
 def upload(request):
     desc = request.POST['description']
     brnd = request.POST['brand']
+    vers = request.POST['version']  
+    mode = request.POST['model']  
 
     if not request.method == 'POST':
         return HttpResponse("POST only")
@@ -67,9 +72,19 @@ def upload(request):
     handle_uploaded_file(f, path)
     md5 = io_md5(path)
 
-    #TODO, Add product...
     image = Image(filename=f.name,description=desc,brand_id=get_brand(brnd),hash=md5, rootfs_extracted=False, kernel_extracted=False)
     image.save()
+    product = Product(iid=image,product=mode,version=vers)
+    product.save()
+    
+    print image
+    print product
+    ./sources/extractor/extractor.py -b Netgear -sql 127.0.0.1 -np -nk "WNAP320 Firmware Version 2.0.3.zip" images
+
+    #Namespace(brand='Netgear', input='../uploads/firmtest.zip', kernel=False, output='/home/unkn0wn/Desktop/', parallel=False, rootfs=True, sql='127.0.0.1')
+    #sry for hardcoded stuff, was just trying. I'll fix that later
+    extract = Extractor('/home/unkn0wn/Desktop/firminator_backend/uploads/firmtest.zip', '/home/unkn0wn/Desktop/', True, False, False, '127.0.0.1' ,'NetGear')
+    extract.extract()
 
     return HttpResponse("File uploaded // hash : %s" % md5)
 
