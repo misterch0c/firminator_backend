ca9dfe953842d4cfa40b875eb840469d49f55533
Shinxs <bram.hoven@hotmail.com>
2016-05-08 12:44:53 +0200

    Get the firmware ready after uploading

--- uploads/watcher.py
+++ uploads/watcher.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 import time
+from django.conf import settings
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 from subprocess import check_call
@@ -7,11 +8,11 @@ from subprocess import check_call
 class FileCreateEvent(FileSystemEventHandler):
     def on_created(self, event):
         brand = "Netgear"
-        number = check_output("python /home/dev/firmadyne/sources/extractor/extractor.py -b " + brand + " -sql 127.0.0.1 -np -nk '" + event.src_path.replace("./", "") + "' images")
-        check_call("/home/dev/firmadyne/scripts/getArch.sh /home/dev/firmadyne/images/" + number + ".tar.gz")
-        check_call("/home/dev/firmadyne/scripts/tar2db.py -i 1 -f /home/dev/firmadyne/images/" + number + ".tar.gz")
-        check_call("sudo /home/dev/firmadyne/scripts/makeImage.sh " + number)
-        check_call("/home/dev/firmadyne/scripts/inferNetwork.sh " + number)
+        number = check_output("python " + settings.FIRMADYNE_DIR + "/sources/extractor/extractor.py -b " + brand + " -sql 127.0.0.1 -np -nk '" + event.src_path.replace("./", "") + "' images")
+        check_call(settings.FIRMADYNE_DIR + "/scripts/getArch.sh /home/dev/firmadyne/images/" + number + ".tar.gz")
+        check_call(settings.FIRMADYNE_DIR + "/scripts/tar2db.py -i 1 -f " + settings.FIRMADYNE_DIR + "/images/" + number + ".tar.gz")
+        check_call("sudo " + settings.FIRMADYNE_DIR + "/scripts/makeImage.sh " + number)
+        check_call(settings.FIRMADYNE_DIR + "/scripts/inferNetwork.sh " + number)
 
 
 if __name__ == "__main__":
