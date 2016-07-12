32c21d6eadcc0219dc8bb206f43056398abc9060
Ganapati <ganapati@foobar.baz>
2016-07-12 11:25:53 +0000

    Add comment + rewrite istext

--- myapp/views.py
+++ myapp/views.py
@@ -40,28 +40,18 @@ import string
         #          ----------'---'
 
 def handle_uploaded_file(f, path):
+    """ Write f content to destination (path)
+    """
     with open(path, 'wb+') as destination:
         for chunk in f.chunks():
             destination.write(chunk)
 
 def isText(filename,iid):
-    s=open("/tmp/"+str(iid)+filename).read(512)
-    text_characters = "".join(map(chr, range(32, 127)) + list("\n\r\t\b"))
-    _null_trans = string.maketrans("", "")
-    if not s:
-        # Empty files are considered text
-        return True
-    if "\0" in s:
-        # Files with null bytes are likely binary
-        return False
-    # Get the non-text characters (maps a character to itself then
-    # use the 'remove' option to get rid of the text characters.)
-    t = s.translate(_null_trans, text_characters)
-    # If more than 30% non-text characters, then
-    # this is considered a binary file
-    if float(len(t))/float(len(s)) > 0.30:
-        return False
-    return True
+    """ Check if file content is printable (text)
+    """
+    with open("/tmp/"+str(iid)+filename) as fd:
+        data = fd.read(512)
+        return all(c in string.printable for c in data)
 
 
 def get_brand(brand):
